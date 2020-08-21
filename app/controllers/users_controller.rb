97b3d963ba424c1515a789008092ee3e392aa0be
02masaki <69668107+02masaki@users.noreply.github.com>
2020-08-21 17:53:04 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+
   def edit
-    
   end
+
   def update
-    if current_user.update(user_parmas)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
